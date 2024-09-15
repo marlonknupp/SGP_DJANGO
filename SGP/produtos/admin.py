ec39cc62dd4f85d3f63bdb809d38c556c9ea4a0e
Marlon Knupp <marlonjcc23@gmail.com>
2024-09-15 20:37:15 -0300

    ...

--- SGP/produtos/admin.py
+++ SGP/produtos/admin.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import Brand , Category, Product
 
@@ -19,3 +21,18 @@ class ProductAdmin(admin.ModelAdmin):
                     'is_active','created_at','updated_at')
     search_fields = ('title','brand_name','category_name')
     list_filter = ('is_active','brand','category')
+
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="products.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['titulo','marca', 'categoria', 'preço',
+                          'ativo','descricao','criado em', 'atualizado em'])
+        for product in queryset:
+            writer.writerow([product.title, product.brand.name, product.category.name,
+                              product.price, product.is_active, product.description,
+                              product.created_at, product.updated_at])
+            return response
+        
+    export_to_csv.short_description = 'Exportar para CSV'
+    actions = [export_to_csv]
